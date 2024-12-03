d7a679110ac0c6b01802f401bd42d93e798784cb
Karim Elnekheily <karim@example.com>
2024-12-03 04:53:47 +0200

    Updated parser 2

--- Parser.py
+++ Parser.py
@@ -29,17 +29,10 @@ def peek_Value():
     return None
            
 def next_token():
-        #global c_token_index
-        #if c_token_index < len(list_of_tokens):
         if list_of_tokens:
-            #token=list_of_tokens[c_token_index]
-            #token=list_of_tokens.pop(0)
+
             list_of_tokens.pop(0)
-            #c_token_index += 1
-            #return token
-        #return None
-#c_token_index = 0
-#match function
+
 def match(current_token_type, expected_token_type):
     
     return current_token_type == expected_token_type
@@ -54,16 +47,10 @@ def program():
   #statement{;statement}   
 def stmtSequence():
     temp=statement();
-    root=temp;
-    #hena an check en statement di is valid
-    #if temp.data!="ERROR":
-        #root=temp
-    #else:
-        #return "ERROR"  
+    root=temp;  
     while True:
         if is_empty(list_of_tokens()):
-            break
-        #hena token_d 3ebara 3an value w type fa value betet5azen f token w type f token_type    
+            break   
         token,token_type=list_of_tokens[0] 
         
         if list_of_tokens and  token_type=="SEMICOLON":
@@ -72,14 +59,10 @@ def stmtSequence():
             temp.setNext(siblings)
         else:
             break
-        
     return root
     #statament -> if-stmt | repeat-stmt | assign-stmt |read-stmt |  write-stmt
 def statement():
-    #token_d=next_token()
-    
-    #token,token_type=list_of_tokens[0]
-    
+        
     if peek_Type()=="IF":
         tmp=if_stmt()
     elif peek_Type()=="REPEAT":
@@ -98,17 +81,12 @@ def statement():
 def if_stmt():
     node_tmp=createIfNode()
     
-    #token_d=next_token()
-    #token,token_type=token_d
     next_token()
-    #token_type=peek_Type()
     node_exp=exp()
     
     node_tmp.addChild(node_exp)
     
     if match(peek_Type(),"THEN"):
-       # if list_of_tokens:
-            #token_d=next_token()
         next_token()
         
         stmt_node=stmtSequence()
@@ -117,13 +95,13 @@ def if_stmt():
         return "ERROR"
     
     if match(peek_Type(),"ELSE"):
-        #token_d=next_token()
+        
         next_token()
         stmt_node=stmtSequence()
         node_tmp.addChild(stmt_node)
           
     if match(peek_Type(),"END"):
-        #token_d=next_token()
+    
         next_token()
     else: 
         return "ERROR"
@@ -199,23 +177,19 @@ def factor():
     else:
         return "ERROR"
 
-    # Return the node or error
     return node_temp
 
-
-# Placeholder functions for repeat, read, write, and assign statements
 #repeat->stmt-sequence until exp
 def repeat_stmt():
     r_node=createRepeatNode()
-    #token_d=next_token()
+   
     next_token()
-    #token,token_type=token_d
+    
     token_type=peek_Type()
     
     b_node=stmtSequence()
     r_node.addChild(b_node)
     if match(token_type,"UNTIL"):
-        #token_d=next_token()
         next_token()
         e_node=exp()
         r_node.addChild(e_node)
@@ -228,9 +202,9 @@ def repeat_stmt():
 #read - > read identifer
 def read_stmt():
     read_stmt =createReadNode()
-    #token_d=next_token()
+    
     next_token()
-    #token,token_type=token_d
+    
     token_type=peek_Type()
     if match(token_type,"IDENTIFIER"):
         read_stmt.addChild(createIDNode(peek_Value()))
@@ -241,7 +215,6 @@ def read_stmt():
 #write -> write exp
 def write_stmt():
     w_root=createWriteNode()
-    #token_d=next_token()
     next_token()
     expNode=exp()
     w_root.addChild(expNode)
@@ -251,22 +224,14 @@ def write_stmt():
 #assignStmt -> Identifier := exp
 def assign_stmt():
     assign_node=createAssignNode("assign") 
-    #token_d=next_token
-    #token,token_type=token_d
+
     token_type=peek_Type()
     if match(token_type,"IDENTIFIER"):
-        #assign_node.addChild(createIDNode(peek_Value()))
         assign_node.data = createIDNode(peek_Value())
         next_token()
-    
-        #return "ERROR"
-        #token_d=next_token()
-        #token,token_type=token_d
         token_type=peek_Type()
         if match(token_type,"ASSIGN"):
-            #token_d=next_token()
             next_token()
-            #token,token_type=token_d
             exp_node=exp()
             assign_node.addChild(exp_node)
         else:
